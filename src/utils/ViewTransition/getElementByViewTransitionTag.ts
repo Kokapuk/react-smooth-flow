4c5c85959bd708ada13fe79efa49f226ac704806
Yaroslav <yarik.pavlov.971@gmail.com>
2024-09-20 18:27:11 +0300

    - Added error when more than one target for view transition tag found

--- src/utils/ViewTransition/getElementByViewTransitionTag.ts
+++ src/utils/ViewTransition/getElementByViewTransitionTag.ts
@@ -1,4 +1,11 @@
-const getElementByViewTransitionTag = (tag: string, parent: Element | Document = document) =>
-  parent.querySelector(`[data-viewtransition*='tag":"${tag}"']`);
+const getElementByViewTransitionTag = (tag: string, parent: Element | Document = document) => {
+  const targets = parent.querySelectorAll(`[data-viewtransition*='tag":"${tag}"']`);
+
+  if (targets.length > 1) {
+    throw new Error(`Found more than one target for tag: ${tag}`);
+  }
+
+  return targets[0];
+};
 
 export default getElementByViewTransitionTag;
